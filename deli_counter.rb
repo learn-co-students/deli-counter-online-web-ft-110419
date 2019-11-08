3930c7c732e95db86d1e1016db56793fddcc3388
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-08 16:25:50 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,11 @@ def line(katz_deli)
   if katz_deli.length < 1
     puts "The line is currently empty."
   else katz_deli.length >= 1
-    statement = "The line is currently:"
+    statement = "The line is currently: "
     katz_deli.each.with_index(1) do |customer,index|
-   y = "#{index}. #{customer}"
-  puts statement.concat(y)
+   number = "#{index}. #{customer} "
+   statement << number
+   puts statement
 end
 end
   end
